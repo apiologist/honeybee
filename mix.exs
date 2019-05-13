73063e4058ce0393144583dae33ce813e39aa818
Simon Finnman <simonfinnman@hotmail.com>
2019-05-13 20:33:35 +0200

    Bump version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Honeybee.MixProject do
   def project do
     [
       app: :honeybee,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: [],
