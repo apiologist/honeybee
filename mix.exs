03e63b71acef7ce1d171450dd1170450e79132d4
sfinnman <simon.finnman@hotmail.com>
2019-08-25 11:51:13 +0200

    0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Honeybee.MixProject do
   def project do
     [
       app: :honeybee,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: [],
